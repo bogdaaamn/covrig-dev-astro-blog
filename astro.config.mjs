c3e2a8588d95c14b14dd819e7951f2346e71cd4b
Bogdan Covrig <bogcov@gmail.com>
2024-01-12 20:50:31 +0100

    chore: Add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   adapter: vercel(),
   output: "server",
 });
